f7e22039ca375efde0b4d3acb569ebcc1cb224a1
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2020-04-01 23:18:42 +0200

    examples/simple.ml: cleanup using function closures to pass extra state

--- examples/simple.ml
+++ examples/simple.ml
@@ -8,8 +8,6 @@ type state = {
 
   new_output : Wl.Listener.t;
   new_input : Wl.Listener.t;
-
-  display : Wl.Display.t option; (* argh *)
 }
 
 type output = {
@@ -27,14 +25,7 @@ let state = ref {
   last_frame = Mtime_clock.now (); dec = 0; color = [|1.; 0.; 0.|];
   new_output = Wl.Listener.create ();
   new_input = Wl.Listener.create ();
-  display = None;
 }
-
-module OutputH = Hashtbl.Make (Output)
-let outputs : output OutputH.t = OutputH.create 25
-
-module KeyboardH = Hashtbl.Make (Input_device)
-let keyboards : keyboard KeyboardH.t = KeyboardH.create 25
 
 let output_frame_notify _ output =
   let st = !state in
@@ -57,41 +48,35 @@ let output_frame_notify _ output =
   ignore (Output.commit output : bool);
   state := { st with last_frame = Mtime_clock.now () }
 
-let output_remove_notify _ output =
-  let o = OutputH.find outputs output in
+let output_remove_notify output_handles _ _output =
   print_endline "Output removed";
-  Wl.Listener.detach o.frame;
-  Wl.Listener.detach o.destroy;
-  OutputH.remove outputs output
+  Wl.Listener.detach output_handles.frame;
+  Wl.Listener.detach output_handles.destroy
 
 let new_output_notify _ output =
   let o = { frame = Wl.Listener.create ();
             destroy = Wl.Listener.create (); } in
   Output.set_best_mode output;
   Wl.Signal.add (Output.signal_frame output) o.frame output_frame_notify;
-  Wl.Signal.add (Output.signal_destroy output) o.destroy output_remove_notify;
-  OutputH.add outputs output o;
+  Wl.Signal.add (Output.signal_destroy output) o.destroy
+    (output_remove_notify o);
   ignore (Output.commit output : bool);
   ()
 
-let keyboard_destroy_notify _ input =
-  let k = KeyboardH.find keyboards input in
+let keyboard_destroy_notify keyboard_handles _ _input =
   print_endline "keyboard removed";
-  Wl.Listener.detach k.key;
-  Wl.Listener.detach k.destroy;
-  KeyboardH.remove keyboards input
+  Wl.Listener.detach keyboard_handles.key;
+  Wl.Listener.detach keyboard_handles.destroy
 
-let keyboard_key_notify keyboard _ event =
+let keyboard_key_notify display keyboard _ event =
   let keycode = Keyboard.Event_key.keycode event + 8 in
   let syms = Xkbcommon.State.key_get_syms (Keyboard.xkb_state keyboard)
       keycode in
-  let display = match !state.display with
-      None -> assert false | Some display -> display in
   if List.mem Xkbcommon.Keysyms._Escape syms then
     Wl.Display.terminate display;
   ()
 
-let new_input_notify _ (input: Input_device.t) =
+let new_input_notify display _ (input: Input_device.t) =
   match Input_device.typ input with
   | Input_device.Keyboard keyboard ->
     let k = {
@@ -100,9 +85,9 @@ let new_input_notify _ (input: Input_device.t) =
       destroy = Wl.Listener.create ();
     } in
     Wl.Signal.add (Input_device.signal_destroy input) k.destroy
-      keyboard_destroy_notify;
+      (keyboard_destroy_notify k);
     Wl.Signal.add (Keyboard.signal_key keyboard) k.key
-      (keyboard_key_notify keyboard);
+      (keyboard_key_notify display keyboard);
     let rules = Xkbcommon.Rule_names.{
       rules = Sys.getenv_opt "XKB_DEFAULT_RULES";
       model = Sys.getenv_opt "XKB_DEFAULT_MODEL";
@@ -124,21 +109,19 @@ let new_input_notify _ (input: Input_device.t) =
     in
     ignore (Keyboard.set_keymap keyboard keymap : bool);
     Xkbcommon.Keymap.unref keymap;
-    Xkbcommon.Context.unref context;
-    KeyboardH.add keyboards input k
+    Xkbcommon.Context.unref context
   | _ ->
     ()
 
 let () =
   Log.(init Debug);
   let display = Wl.Display.create () in
-  state := { !state with display = Some display };
   let backend = Backend.autocreate display in
 
   Wl.Signal.add (Backend.signal_new_output backend) (!state).new_output
     new_output_notify;
   Wl.Signal.add (Backend.signal_new_input backend) (!state).new_input
-    new_input_notify;
+    (new_input_notify display);
 
   if not (Backend.start backend) then (
     Backend.destroy backend;
